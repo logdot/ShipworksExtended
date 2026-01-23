553fc830e3d35817f2341969f85798125574ac38
Timoteo Rider Sweeney <timoteo2.25@gmail.com>
2026-01-22 19:53:19 -0600

    Add mingw-toolchain

---
+++ mingw-toolchain.cmake
@@ -0,0 +1,14 @@
+# MinGW-w64 Cross-Compilation Toolchain File
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# Specify the cross-compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# Target environment
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
